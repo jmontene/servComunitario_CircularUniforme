99cb51847d1f5e98330f7a85f2600a2b575ebf6d
Gamar <gamarazuaje@gmail.com>
2013-11-27 03:01:30 -0430

    Comandos terminados

--- Chat/Commands/clientData.h
+++ Chat/Commands/clientData.h
@@ -1,16 +1,33 @@
 #define MAX_SIZE 50
 
+/**
+ * Modulo que define los tipos cliente y sala.
+ * Las cuales seran usados por el manager.
+ * 
+ * Implementa funciones auxiliares, a ser usadas
+ * por ambos tipos.
+ */
 
+
+/**
+ * Tipo cliente
+ * Posee nombre y la lista de nombres 
+ * de sala al cual pertenece
+ */
 typedef struct Cliente
 {
    char  name[MAX_SIZE];
    char** salas;
    int numSalas; //Current number of rooms
    int maxSalas; //Max number of rooms
-   int descriptor;
    
 } cliente;
 
+/**
+ * Tipo sala
+ * Posee nombre y la lista de nombres 
+ * de clientes que pertenecen a ella.
+ */
 typedef struct Sala
 {
   char name[MAX_SIZE];
@@ -19,17 +36,89 @@ typedef struct Sala
   int maxClientes;
 } sala;
 
+/**
+ * Inicializador del tipo cliente.
+ * Inicializa su lista de salas y su nombre.
+ * @param clname nombre del cliente
+ * @return cliente que fue inicializado
+ */
+cliente* cliente_crear(char *clname);
 
-cliente* cliente_crear(char *clname, int des);
+/**
+ * Agrega una sala a un cliente.
+ * Imprime un mensaje de error si el cliente posee dicha sala.
+ * @param cl cliente a ser modificado
+ * @param sala a agregar
+ */
 void cliente_agregarSala(cliente *cl, char *sala);
+
+/**
+ * Elimina una sala del cliente
+ * Imprime error si no existe
+ * @param cl cliente a modificar
+ * @param sala a eliminar
+ */
 void cliente_eliminarSala(cliente *cl, char *sala);
+
+/**
+ * Elimina todas las salas de un cliente
+ * Imprime error si no existe
+ * @param cl cliente a modificar
+ */
 void cliente_borrarSalas(cliente *cl);
+
+/**
+ * Imprime la informacion del cliente
+ * @param cl cliente a imprimir
+ */
 void cliente_imprimir(cliente *cl);
+
+/**
+ * Elimina el cliente y toda su informacion.
+ * @param cl cliente a eliminar
+ */
 void cliente_eliminar(cliente *cl);
 
+/**
+ * Inicializador del tipo sala.
+ * Inicializa su lista de clientes y su nombre.
+ * @param sname nombre de la sala
+ * @return sala que fue inicializada
+ */
 sala* sala_crear(char *sname);
+
+/**
+ * Agrega un cliente a una sala
+ * Imprime un mensaje de error si en la sala
+ * ya se encuentra dicho cliente.
+ * @param s sala a ser modificada
+ * @param cliente a agregar
+ */
 void sala_agregarCliente(sala *s, char *cliente);
+
+/**
+ * Elimina un cliente de una sala
+ * Imprime error si no existe
+ * @param s sala a modificar
+ */
 void sala_eliminarCliente(sala *s, char *cliente);
+
+/**
+ * Elimina todos los clientes de una sala
+ * Imprime error si no existe
+ * @param cl cliente a modificar
+ */
 void sala_borrarClientes(sala *s);
+
+/**
+ * Imprime la informacion de la sala
+ * @param s sala a imprimir
+ */
 void sala_imprimir(sala *s);
+
+/**
+ * Elimina la sala y toda su informacion.
+ * @param s sala a eliminar
+ */
 void sala_eliminar(sala *s);
+
--- Chat/Commands/commandManager.h
+++ Chat/Commands/commandManager.h
@@ -2,6 +2,13 @@
 #include <stdlib.h>
 #include "clientData.h"
 
+/**
+ * Modulo que implementa las funcionalidades 
+ * de los comandos del chat. Manager
+ * 
+ * Posee un arreglo de clientes y de salas.
+ * Y lo modifica de acuerdo a las funciones. 
+ */
 
 typedef struct Manager
 {
@@ -9,7 +16,7 @@ typedef struct Manager
   sala** salas;
   
   
-  //limits number of rooms and clients
+  //Indica el numero de salas y clientes.
   int numSalas;
   int maxSalas;
   
@@ -18,14 +25,101 @@ typedef struct Manager
   
 } manager;
 
+/**
+ * Inicializador del modulo.
+ * Inicializa la lista de sala y de clientes.
+ * Reserva su espacio e inicializa su informacion.
+ * Posee una lista de clientes, y una lista de salas.
+ * @return mng manager que fue inicializado
+ */
 manager* manager_crear();
-void manager_agregarCliente(manager *mng, char* clname, int fd);
+
+/**
+ * Agrega un cliente al manager.
+ * Imprime un mensaje de error si ya existe.
+ * @param mng manager a ser modificado
+ * @param clname nombre del cliente a agregar
+ */
+void manager_agregarCliente(manager *mng, char* clname);
+
+/**
+ * Agrega una sala al manejador
+ * Imprime un mensaje de error si ya existe.
+ * @param mng manager a ser modificado
+ * @param sname nombre de la sala a agregar
+ */
 void manager_agregarSala(manager *mng, char* sname);
-int manager_suscribirCliente(manager *mng, char *clname, char * sname);
-void manager_desuscribirCliente(manager *mng, char *clname);
-void manager_eliminarCliente(manager *mng, char *clname);
-void manager_eliminarSala(manager *mng, char* sname);
-void manager_mostrarClientes(manager *mng);
-void manager_mostrarSalas(manager *mng);
+
+/**
+ * Busca un cliente en el manejador
+ * Retorna su posicion en el arreglo. 
+ * Retorna -1 si no existe.
+ * @param mng manager a ser buscado
+ * @param clname nombre del cliente a buscar
+ */
 int manager_buscarCliente(manager *mng, char *clname);
+
+/**
+ * Busca una sala en el manejador
+ * Retorna su posicion en el arreglo. 
+ * Retorna -1 si no existe.
+ * @param mng manager a ser buscado
+ * @param sname nombre de la sala a buscar
+ */
 int manager_buscarSala(manager *mng, char *sname);
+
+/**
+ * Suscribe un cliente a una sala.
+ * Imprime error si el cliente o la sala no existen.
+ * Imprime error si el cliente ya se encontraba suscrito
+ * a dicha sala.
+ * @param mng manager a modificar
+ * @param clname nombre del cliente a suscribir
+ * @param sname nombre de la sala a la cual suscribir el cliente.
+ */
+void manager_suscribirCliente(manager *mng, char *clname, char * sname);
+
+/**
+ * Desuscribe un cliente de todas las salas.
+ * Imprime error si el cliente no existe o
+ * si no se encuentra suscrito a ninguna sala.
+ * @param mng manager a modificar
+ * @param clname nombre del cliente a desuscribir
+ */
+void manager_desuscribirCliente(manager *mng, char *clname);
+
+/**
+ * Elimina un cliente del manejador
+ * Imprime error si no existe.
+ * @param mng manager a modificar
+ * @param clname nombre del cliente a eliminar
+ */
+void manager_eliminarCliente(manager *mng, char *clname);
+
+/**
+ * Elimina una sala del manejador
+ * Imprime error si no existe.
+ * @param mng manager a modificar
+ * @param sname nombre de la sala a eliminar
+ */
+void manager_eliminarSala(manager *mng, char* sname);
+
+/**
+ * Muestra todos los clientes del manager
+ * @param mng manager a consultar
+ */
+void manager_mostrarClientes(manager *mng);
+
+/**
+ * Muestra todas las salas del manager
+ * @param mng manager a consultar
+ */
+void manager_mostrarSalas(manager *mng);
+
+/**
+ * Elimina un manager
+ * @param mng manager a eliminar
+ */
+void manager_eliminar(manager *mng);
+
+
