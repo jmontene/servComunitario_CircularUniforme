5802d03216ee9e199da7b79e24b94f1f0b5cb796
Luis Miranda <lcmm1993@gmail.com>
2014-08-07 02:17:32 -0430

    Fuertes cambios en la implementacion de Ally y Enemy, se usan las funciones de Phaser. Solucionado problema de angulo inicial

--- Game/level1.js
+++ Game/level1.js
@@ -20,10 +20,9 @@ Game.level1.prototype = {
 	
 	
 	//Crea el enemigo
-	enemy = new Enemy('enemy',50,50,5,earth,this.game);
+	enemy = new Enemy('enemy',400,50,5,earth,this.game);
 	enemy.sprite.anchor.setTo(0.5,0.5);
 	enemy.sprite.scale.setTo(0.25,0.25);
-	
 	this.game.physics.enable(enemy.sprite,Phaser.Physics.ARCADE);
 	enemy.sprite.body.collideWorldBounds = true;
 	
@@ -33,19 +32,16 @@ Game.level1.prototype = {
 	
 	//Crear el sprite de la ultraball de la misma forma, excepto que su posicion
 	//Y depende del radio
-	ball = new Ally('ball',0.12,200,earth,this.game);
+	ball = new Ally('ball',300,375,0.12,earth,this.game);
 	ball.sprite.anchor.setTo(0.5,0.5);
 	ball.sprite.scale.setTo(0.05,0.05);
 	this.game.physics.enable(ball.sprite,Phaser.Physics.ARCADE);
-	ball.sprite.body.collideWorldBounds = true;
-	ball.set_fix(ball.sprite.body.center.x-earth.body.center.x,
-		     ball.sprite.body.center.y-earth.body.center.y)    
-	
+	ball.initialize();    
+
 	console.log("this.game center, x = %d y = %d",this.game.world.centerX,this.game.world.centerY)
 	console.log("blue ball center, x = %d y = %d",earth.body.center.x,earth.body.center.y)
 	console.log("ultra ball center, x = %d y = %d",ball.sprite.body.center.x,ball.sprite.body.center.y)
-	
-	
+		
     },
 
     update: function(){
@@ -53,17 +49,18 @@ Game.level1.prototype = {
 	if(play){
 	    this.time ++;
 	    enemy.move();
-	    
 	    result = "..."
 	}else{
-	    this.time = 1;
+	    this.time = 0;
 	    enemy.reset();
 	    ball.change_radio(dummyradio);
 	}
-	ball.move(this.time);
-	this.game.physics.arcade.collide(earth, enemy.sprite, collide_earth, null, this);
-	this.game.physics.arcade.collide(ball.sprite, enemy.sprite, collide_ally, 
-				    null, this);
+	ball.move(this.time);	    
+	
+	this.game.physics.arcade.collide(earth, enemy.sprite, collide_earth, 
+					 null, this);
+	this.game.physics.arcade.collide(ball.sprite, enemy.sprite, collide_ally,
+					 null, this);
 	this.game.debug.text(result,300,50);
     }
 }
